4cd1d2f06718e32693c7791b71fcb3d6a7c0e871
崔航 <562138306@qq.com>
2023-05-09 18:14:20 +0800

    generateImage

--- pages/api/nft/generate_image.js
+++ pages/api/nft/generate_image.js
@@ -2,17 +2,28 @@ import {completions} from '../../utils/chat_gpt'
 import {generate} from '../../utils/dalle2'
 
 const handler = async (req, res) => {
-
-    const {prompt} = req.body
+    let body = req.body
+    if (typeof req.body === 'string') {
+        body = JSON.parse(req.body)
+    }
+    const {prompt} = body
     const messages = [
         {
+            role: 'system',
+            content: '请严格按照我后续提问的要求给出回答，不要用问题回答问题，直接给出你认为最合适的答案，不要其他任何描述'
+        },
+        {
             role: 'user',
-            content: '请严格按照我后续提问的要求给出回答，不要用问题回答问题，直接给出你认为最合适的答案，不要其他任何描述，不要Prompt前缀，为以下描述给出dalle2的英文版提示Prompt：' + prompt
+            content: '为以下描述给出dalle2的英文版提示Prompt，不要Prompt前缀：' + prompt
         }
     ]
     const dalle2Prompt = await completions(messages)
+    const generatedImages = await generate(dalle2Prompt, 3)
+    for (let i = 0; i < generatedImages.length; i++) {
+        generatedImages[i] = 'data:image/png;base64,' + generatedImages[i]
+    }
     const result = {
-        'images': await generate(dalle2Prompt, 3)
+        'images': generatedImages
     }
     res.status(200).json(result)
 }
